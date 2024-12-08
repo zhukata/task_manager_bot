f3b5b5e706019bccd2a2d1d6b8ce955ac674743f
zhukata <zhukata@gmail.com>
2024-12-09 00:06:48 +0300

    my_base_db

--- filters/chat_types.py
+++ filters/chat_types.py
@@ -2,6 +2,8 @@ from aiogram.filters import Filter
 from aiogram import Bot, types
 
 import api
+from database.orm_query import orm_get_user
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class ChatTypeFilter(Filter):
@@ -16,5 +18,7 @@ class IsAdmin(Filter):
     def __init__(self):
         pass
 
-    async def __call__(self, message: types.Message, bot: Bot):
-        return message.from_user.id in api.user_tokens
+    async def __call__(self, message: types.Message, session: AsyncSession):
+        if not orm_get_user(session, message.from_user.id):
+            return False
+        return True
